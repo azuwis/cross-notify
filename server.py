ca933a9f39dc41b20d612b37a07115a0f0b8ccb6
Zhong Jianxin <azuwis@gmail.com>
2014-09-05 16:08:29 +0800

    Add platform dependent code

--- server.py
+++ server.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from subprocess import call
+import sys
 import urllib
 
 PORT_NUMBER = 45678
+
+class Notify:
+
+    def __init__(self):
+        pf = sys.platform
+        if pf.startswith('linux') or pf.startswith('freebsd'):
+            self.notify = self.__notify_x
+        elif pf.startswith('darwin'):
+            import platform
+            v, _, _ = platform.mac_ver()
+            v = float('.'.join(v.split('.')[:2]))
+            if v >= 10.8:
+                self.notify = self.__notify_osx_ge_10_8
+            else:
+                self.notify = self.__notify_osx_lt_10_8
+        else:
+            print('Platform not supported.')
+            sys.exit(1)
+
+    def __notify_x(self, msg):
+        call(['notify-send', msg])
+
+    def __notify_osx_ge_10_8(self, msg):
+        call(['osascript', '-e', 'display notification "%s"' % msg])
+
+    def __notify_osx_lt_10_8(self, msg):
+        call(['growlnotify', '-m', msg])
+
+notify = Notify()
 
 class Handler(BaseHTTPRequestHandler):
 
@@ -15,7 +45,7 @@ class Handler(BaseHTTPRequestHandler):
         # XXX
         title = urllib.unquote(self.path.lstrip('/?'))
         if title:
-            call(['notify-send', title])
+            notify.notify(title)
         return
 
 try:
